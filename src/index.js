165f78c87a63152e57a369399b2967b102000d90
Avantika11201 <avantikajewani.bscsf21@iba-suk.edu.pk>
2023-12-11 23:23:41 +0500

    final commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { Header } from './Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Herosection } from './components/Herosection';
+import { Cards } from './components/Cards';
+import { Benifits } from './components/Benifits';
+import { Contact } from './components/Contact';
+import { Footer } from './components/Footer';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <>
+    <Header/>
+    <Herosection />
+    <Cards/>
+    <Benifits/>
+    <Contact/>
+    <Footer/>
+  </>
+    
+    
+);
+
+
